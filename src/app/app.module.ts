c18d90137e83b8673c2bf3ee41f022b33916a065
dat.phan <dat.phan@orientsoftware.com>
2021-04-08 17:12:41 +0700

    modify theme and feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,11 @@ import { DrinkComponent } from './components/drink/drink.component';
 import { CommonModule } from '@angular/common';
 import { RecipeComponent } from './components/recipe/recipe.component';
 import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -36,6 +41,10 @@ import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.c
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/data/drink-types.ts
+++ src/app/data/drink-types.ts
@@ -2,12 +2,25 @@ import { DrinkType } from '../models/drink-type.model';
 
 export const drinkTypes: DrinkType[] = [
     {
-        type: 'Virgin Mojito',
-        slug: 'virgin-mojito'
+        type: 'Thức uống truyền thống',
+        slug: 'truyen-thong'
     },
     {
         type: 'Cà phê',
         slug: 'ca-phe'
+    },
+    {
+        type: 'Trà',
+        slug: 'tra'
+    }
+    ,
+    {
+        type: 'Nước ép',
+        slug: 'nuoc-ep'
+    },
+    {
+        type: 'Nước ép đa tầng',
+        slug: 'nuoc-ep-da-tang'
     },
     {
         type: 'Sinh tố Việt',
@@ -18,11 +31,34 @@ export const drinkTypes: DrinkType[] = [
         slug: 'sinh-to-au'
     },
     {
-        type: 'Soda Italian',
-        slug: 'soda-italian'
+        type: 'Ice blended coffee',
+        slug: 'ice-blended-coffee'
+    },
+    {
+        type: 'Mill shaker',
+        slug: 'mill-shaker'
+    },
+    {
+        type: 'Yoghurt',
+        slug: 'yoghurt'
+    },
+    {
+        type: 'Sữa chua',
+        slug: 'sua-chua'
+    }, {
+        type: 'Virgin Mojito',
+        slug: 'virgin-mojito'
+    },
+    {
+        type: 'Italian Soda',
+        slug: 'italian-soda'
     },
     {
         type: 'Trà sữa',
         slug: 'tra-sua'
     },
+    {
+        type: 'Thức uống theo xu hướng',
+        slug: 'xu-huong'
+    }
 ];
