fe3f9623984b121318430c86451fa5cab9482af3
dat.phan <dat.phan@orientsoftware.com>
2021-04-08 17:41:59 +0700

    Moddify css

--- src/app/data/drinks.ts
+++ src/app/data/drinks.ts
@@ -16,5 +16,20 @@ export const drinks: Drink[] = [
             , 'Cho đá gần đầy ly -> rót soda vào đầy ly -> khuấy đều'],
         type: 'Virgin Mojito',
         name: 'Virgin Pasion Mojito',
+    }, {
+        img: 'https://www.cairowestmag.com/wp-content/uploads/2018/02/vmojito3-1.jpg',
+        materials: ['Chanh 1 quả', 'Nước chanh 10ml', 'Đường cát 10g', 'Soda', 'Rau húng'],
+        steps: ['Chọn chanh vỏ mỏng cắt làm 4', 'Cho chanh đã cắt vào ly -> cho đường cát vào dầm chung với chanh -> đập rau húng vào cùng các nguyên liệu còn lại'
+            , 'Cho đá gần đầy ly -> rót soda vào đầy ly -> khuấy đều'],
+        type: 'Virgin Mojito',
+        name: 'Virgin Mojito',
+    },
+    {
+        img: 'https://pizzacavicent.es/345-large_default/caipiroska-de-fresa.jpg',
+        materials: ['Chanh 1 quả', 'Syrup chanh dây 30ml', 'Chanh dây 20ml (có hạt, tầm 1 quả)', 'Đường cát 2 muỗng', 'Soda', 'Rau húng'],
+        steps: ['Chọn chanh vỏ mỏng cắt làm 4', 'Cho chanh đã cắt vào ly -> cho đường cát vào dầm chung với chanh -> đập rau húng vào cùng các nguyên liệu còn lại'
+            , 'Cho đá gần đầy ly -> rót soda vào đầy ly -> khuấy đều'],
+        type: 'Virgin Mojito',
+        name: 'Virgin Pasion Mojito',
     }
 ];
